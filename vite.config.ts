41985d616a40e9cd2fe1ca58ff3a3d4e4624922a
Daniyal <daniyal.base@gmail.com>
2026-07-06 06:18:44 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig(() => {
-  return {
-    base: '/',
-    plugins: [react()],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      },
-    },
-  };
-});
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+})
